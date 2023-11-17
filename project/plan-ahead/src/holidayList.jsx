ad3405fe4ee793033cc2592288708247026c0c65
cedricyong00 <cedricyong2000@gmail.com>
2023-11-18 00:02:58 +0800

    Made the table for the event, date and month 17/11

--- project/plan-ahead/src/holidayList.jsx
+++ project/plan-ahead/src/holidayList.jsx
@@ -4,30 +4,47 @@ function HolidayList () {
 
     const [holiday, setHoliday] = useState([]);
 
-    //Month for data filtering
-    let month = 1;
-
     //Holiday API
     const apiUrl = new URL("https://holidayapi.com/v1/holidays")
       apiUrl.search = new URLSearchParams({
         key: "2a4d8a45-d7bc-4753-9748-a86b7841984e",
         country: "SG",
         year: 2022,
-        month: month
-      })
-  
+      });
+
+      // fetches data every time page renders
       useEffect(() => {
         async function fetchHoliday() {
           const response = await fetch(apiUrl);
           const jsonData = await response.json();
-          setHoliday(jsonData);
+          setHoliday(jsonData.holidays);
         }
         fetchHoliday();
       }, []);
 
     return (
         <>
-        <h1>Holidays! Celebrate!</h1>
+        <h1>Upcoming Schedule</h1>
+        <table>
+        <tbody>
+          {holiday.map((holidayElement) => {
+            {/* To convert date string into names of the month and day */}
+            {/* Month */}
+            const date = new Date(holidayElement.observed);
+            const month = date.getMonth();
+            const monthNames = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+          
+            // Date
+            const day = date.getDate();
+            return (
+              <tr key={holidayElement.uuid} className="TableOfDates">
+                <td> {holidayElement.name}</td>
+                <td>{(monthNames[month])} {(day)}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
         </>
     )
 }
