460f893c1beb89f64c7489bc098e5e9d9a7fd104
cedricyong00 <cedricyong2000@gmail.com>
2023-11-23 21:58:52 +0800

    Finished 3 main tables - 23/11

--- project/plan-ahead/src/personalPlan.jsx
+++ project/plan-ahead/src/personalPlan.jsx
@@ -1,33 +1,78 @@
 /* eslint-disable no-unused-vars */
 
 import { useEffect, useState } from "react";
+import PersonalPlanTable from "./personalPlanTable";
 
 function PersonalPlan() {
-  // Weather Data
+  // Table font style
+  const fontStyle = {
+    color: "black",
+    fontSize: "30px",
+  };
+
+  // Table CSS style
+  const divStyle = {
+    border: "2px solid teal",
+    overflow: "auto",
+    height: "300px",
+  };
+
+  // Air Table Data
   const [scheduleData, setScheduleData] = useState([]);
+  const [scheduleID, setScheduleID] = useState("");
 
   //AirTable data for personal schedules
   const apiUrl = new URL(
     "https://api.airtable.com/v0/appq8DCLQI9VU4lCa/Personal%20Schedules?sort%5B0%5D%5Bfield%5D=Date&sort%5B0%5D%5Bdirection%5D=asc"
   );
-  apiUrl.search = new URLSearchParams({
-    api_key: "patC4edhed6aQeaB3.8ce40da4208998c565dcf55153392cc95e1b4d1cd9379f4d5f9e2a380b583425",
-    sort: [{ field: "Date", direction: "asc" }],
-  });
+
+  const url =
+    "https://api.airtable.com/v0/appq8DCLQI9VU4lCa/Personal%20Schedules";
 
   // fetches data every time page renders
   useEffect(() => {
     async function fetchSchedule() {
-      const response = await fetch(apiUrl);
+      const response = await fetch(apiUrl, {
+        headers: {
+          Authorization:
+            "Bearer patC4edhed6aQeaB3.8ce40da4208998c565dcf55153392cc95e1b4d1cd9379f4d5f9e2a380b583425",
+        },
+        method: "GET",
+      });
       const jsonData = await response.json();
-      setScheduleData(jsonData);
+      setScheduleData(jsonData.records);
     }
     fetchSchedule();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   console.log(scheduleData);
-  
-  return <></>;
+
+  // Function to delete record
+  async function deleteSchedule(id) {
+    const response = await fetch(
+      `https://api.airtable.com/v0/appq8DCLQI9VU4lCa/Personal%20Schedules/${id}`,
+      {
+        headers: {
+          Authorization:
+            "Bearer patC4edhed6aQeaB3.8ce40da4208998c565dcf55153392cc95e1b4d1cd9379f4d5f9e2a380b583425",
+        },
+        method: "DELETE",
+      }
+    );
+    const jsonData = await response.json();
+  }
+
+  return (
+    <>
+      <h1 style={fontStyle}>Personal Schedule</h1>
+      <div style={divStyle}>
+        <PersonalPlanTable
+          scheduleData={scheduleData}
+          deleteSchedule={deleteSchedule}
+        />
+      </div>
+    </>
+  );
 }
 
 export default PersonalPlan;
---
+++ project/plan-ahead/src/personalPlanTable.jsx
@@ -0,0 +1,67 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableContainer,
+} from "@chakra-ui/react";
+
+function PersonalPlanTable({ scheduleData, deleteSchedule }) {
+  //Array of days in a week
+  const days = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+
+  function fetchID(id) {
+    deleteSchedule(id);
+  }
+
+  return (
+    <TableContainer>
+      <Table variant="striped" colorScheme="teal">
+        <Thead>
+          <Tr>
+            <Th>Event</Th>
+            <Th>Date</Th>
+            <Th>Day</Th>
+            <Th>Remarks</Th>
+            <Th>Delete</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {scheduleData.map((scheduleElement) => {
+            // Retrieve Day
+            const date = new Date(scheduleElement.fields.Date);
+            const day = date.getDay();
+
+            return (
+              <Tr key={scheduleElement.id}>
+                <Td>{scheduleElement.fields.Title}</Td>
+                <Td> {scheduleElement.fields.Date}</Td>
+                <Td>{days[day]}</Td>
+                <Td>{scheduleElement.fields.Remarks}</Td>
+                <Td>
+                  <button onClick={() => fetchID(scheduleElement.id)}>
+                    DELETE
+                  </button>
+                </Td>
+              </Tr>
+            );
+          })}
+        </Tbody>
+      </Table>
+    </TableContainer>
+  );
+}
+
+export default PersonalPlanTable;
